a0138801bf2ec00d92fccafef003a1474e06974f
Danny Willems <contact@danny-willems.be>
2016-03-29 16:50:40 +0200

    update to gen_js_api version

--- README.md
+++ README.md
@@ -19,7 +19,24 @@ available until after the deviceready event.
 
 Source: [cordova-plugin-device-motion](https://github.com/apache/cordova-plugin-device-motion)
 
+## Repository branches and tags
+
+We are migrating bindings from
+[js_of_ocaml](https://github.com/ocsigen/js_of_ocaml) (low level bindings) to
+[gen_js_api](https://github.com/lexifi/gen_js_api) (high level bindings).
+
+The gen_js_api binding allows to use *pure* ocaml types (you don't have to use
+the ## syntax from js_of_ocaml or Js.string type but only # and string type).
+
+The js_of_ocaml version is available in the branch
+[*js_of_ocaml*](https://github.com/dannywillems/ocaml-cordova-plugin-device-motion/tree/js_of_ocaml)
+but we **recommend** to use the gen_js_api version which is the master branch.
+
 ## How to use ?
+
+You need to have Js_date module which is provided in the binding to the
+javascript standard library you can found
+[here](https://github.com/dannywillems/ocaml-js-stdlib).
 
 See the official documentation
 [cordova-plugin-device-motion](https://github.com/apache/cordova-plugin-device-motion)
@@ -33,13 +50,13 @@ We don't provide a *navigator.accelerometer* variable in this plugin (as said in
 documentation on js_of_ocaml). If we did, *navigator.accelerometer* will be set to **undefined**
 because the *navigator.accelerometer* object doesn't exist when we create the variable.
 
-Instead, we provide a function *Accelerometer.accelerometer* of type unit -> Accelerometer.accelerometer
-Js.t which does the binding when you call it.
+Instead, we provide a function *Accelerometer.t* of type unit ->
+Accelerometer.accelerometer Js.t which does the binding when you call it.
 So, use
 
 ```OCaml
-let on_device_ready =
-  let a = Accelerometer.accelerometer () in
+let on_device_ready _ =
+  let a = Accelerometer.t () in
   (* Some code *)
 
 let _ =
--- accelerometer.mli
+++ accelerometer.mli
@@ -1,32 +1,46 @@
+(* -------------------------------------------------------------------------- *)
 (* Class representing sent data by the accelerometer *)
-class type _Acceleration =
+class acceleration : Ojs.t ->
   object
-    method x : int Js.readonly_prop
-    method y : int Js.readonly_prop
-    method z : int Js.readonly_prop
-    method timestamp : Js.date Js.t
+    inherit Ojs.obj
+    method x          : int
+    method y          : int
+    method z          : int
+    method timestamp  : Js_date.date (* See ocaml-js-stdlib *)
   end
+(* -------------------------------------------------------------------------- *)
 
-type id = int
-type cb_success = _Acceleration Js.t -> unit
-type cb_error = unit -> unit
-
-type options = < frequency : int Js.readonly_prop > Js.t
-
-class type accelerometer =
+(* -------------------------------------------------------------------------- *)
+class options : Ojs.t ->
   object
-    method getCurrentAcceleration :
-      cb_success  ->
-      cb_error    ->
-      unit Js.meth
-    method watchAcceleration :
-      cb_success  ->
-      cb_error    ->
-      options     ->
-      id Js.meth
-    method clearWatch : id -> unit Js.meth
+    inherit Ojs.obj
+    method frequency    : int
   end
+(* -------------------------------------------------------------------------- *)
 
-val create_options : int -> options
+(* -------------------------------------------------------------------------- *)
+class accelerometer : Ojs.t ->
+  object
+    inherit Ojs.obj
+    method get_current_acceleration : (acceleration -> unit)  ->
+                                      (unit -> unit)          ->
+                                      unit
 
+    method watch_acceleration       : (acceleration -> unit)  ->
+                                      (unit -> unit)          ->
+                                      options                 ->
+                                      int
+    method clear_watch              : int                     ->
+                                      unit
+  end
+(* -------------------------------------------------------------------------- *)
+
+(* -------------------------------------------------------------------------- *)
+val create_options : ?frequency:(int [@js.default 1000]) -> options
+[@@js.builder]
+(* -------------------------------------------------------------------------- *)
+
+(* -------------------------------------------------------------------------- *)
 val accelerometer : unit -> accelerometer Js.t
+[@@js.get "navigator.accelerometer"]
+(* -------------------------------------------------------------------------- *)
